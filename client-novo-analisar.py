f3c6d7df078034e2095ef02d45b823b0aa46293b
altieresl <altieresl@outlook.com>
2020-08-19 01:41:51 -0300

    Nova versão de testes adicionada

---
+++ client-novo-analisar.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+import threading
+host = '191.252.186.52'
+porta = 50002
+objSocket = None
+
+def lerMensagens():
+    while True:
+        data = objSocket.recv(1024)
+        print(data.decode())
+
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as objSocket:
+        objSocket.connect((host, porta))
+        threadLerMensagens = threading.Thread(target=lerMensagens)
+        threadLerMensagens.daemon = True
+        threadLerMensagens.start()
+        while True:
+            objSocket.sendall(input().encode())
+except:
+    objSocket.close()
--- client.py
+++ client.py
@@ -1,33 +1,38 @@
 import socket
 import sys
-from aioconsole import ainput
-import asyncio
-HOST = '191.252.186.52'
-PORT = 50000              # The same port as used by the server
+import threading
+host = '191.252.186.52'
+porta = 50002
+objSocket = None
 
-async def lerMensagens():
-    
-s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-    af, socktype, proto, canonname, sa = res
-    try:
-        s = socket.socket(af, socktype, proto)
-    except OSError as msg:
-        s = None
-        continue
-    try:
-        s.connect(sa)
-    except OSError as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print('could not open socket')
-    sys.exit(1)
-with s:
+def lerMensagens():
     while True:
-        entrada = input('Is this your line? ').encode()
-        s.sendall(entrada)
-        data = s.recv(1024)
-        rint('Received', repr(data))
+        data = objSocket.recv(1024)
+        print(data.decode())
+
+try:
+    for res in socket.getaddrinfo(host, porta, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        try:
+            objSocket = socket.socket(af, socktype, proto)
+        except OSError as msg:
+            objSocket = None
+            continue
+        try:
+            objSocket.connect(sa)
+        except OSError as msg:
+            objSocket.close()
+            objSocket = None
+            continue
+        break
+    if objSocket is None:
+        print('Erro ao abrir o socket')
+        sys.exit(1)
+    with objSocket:
+        threadLerMensagens = threading.Thread(target=lerMensagens)
+        threadLerMensagens.daemon = True
+        threadLerMensagens.start()
+        while True:
+            objSocket.sendall(input().encode())
+except:
+    objSocket.close()
